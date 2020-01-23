aad38182e0dc45397099d77c41c13a373e10639d
Thinh Nguyen <37779736+ttngu207@users.noreply.github.com>
2020-01-23 09:06:31 -0600

    minor print format improvement

--- backend/mapapi.py
+++ backend/mapapi.py
@@ -176,12 +176,12 @@ def handle_q(subpath, args, proj, **kwargs):
     if subpath == 'sessionpage':
         recordable_brain_regions = ephys.ProbeInsertion.aggr(ephys.ProbeInsertion.RecordableBrainRegion.proj(
           brain_region='CONCAT(hemisphere, " ", brain_area)'),
-          brain_regions='GROUP_CONCAT(brain_region)', keep_all_rows=True)
+          brain_regions='GROUP_CONCAT(brain_region SEPARATOR", ")', keep_all_rows=True)
 
         sessions = (experiment.Session * lab.WaterRestriction).aggr(
           ephys.ProbeInsertion, 'session_date', 'water_restriction_number', 'username',
           probe_count='count(insertion_number)', keep_all_rows=True).aggr(
-          recordable_brain_regions, ..., insert_locations='GROUP_CONCAT(brain_regions)', keep_all_rows=True)
+          recordable_brain_regions, ..., insert_locations='GROUP_CONCAT(brain_regions SEPARATOR", ")', keep_all_rows=True)
 
         sessions = sessions.aggr(ephys.Unit, ..., clustering_methods='GROUP_CONCAT(DISTINCT clustering_method)', keep_all_rows=True)
         sessions = sessions.aggr(report.SessionLevelReport, ..., behavior_performance_s3fp='behavior_performance', keep_all_rows=True)
@@ -204,7 +204,7 @@ def handle_q(subpath, args, proj, **kwargs):
         probe_insertions = (ephys.ProbeInsertion * ephys.ProbeInsertion.InsertionLocation).proj(
           ..., *exclude_attrs).aggr(ephys.ProbeInsertion.RecordableBrainRegion.proj(
           brain_region='CONCAT(hemisphere, " ", brain_area)'), ...,
-          brain_regions='GROUP_CONCAT(brain_region)', keep_all_rows=True)
+          brain_regions='GROUP_CONCAT(brain_region SEPARATOR", ")', keep_all_rows=True)
 
         probe_insertions = probe_insertions.aggr(
           report.ProbeLevelReport, ..., clustering_quality_s3fp='clustering_quality',
